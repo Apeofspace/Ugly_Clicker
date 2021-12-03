d37caf415603ff6e5533aba184c4353cc7fa23e6
Apeofspace <sintrael@gmail.com>
2021-12-03 16:38:16 +0300

    WIP friday

--- events.py
+++ events.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 import tkinter.ttk as ttk
+import keyboard
 
 
 class MainFrame(tk.Frame):
@@ -13,57 +14,144 @@ class EventManager:
         self.event_list = {}
         self.parent_window = parent_window
         self.event_counter = 0
+        keyboard.on_press(self.on_key_press_callback)
 
     def add_event(self):
         self.event_counter += 1
-        # self.event_list.append(EventFrame(self.parent_window, self, self.event_counter))
-        self.event_list[self.event_counter] = EventFrame(self.parent_window, self, self.event_counter)
+        self.event_list[self.event_counter] = Event(self.parent_window, self, self.event_counter)
 
     def remove_event(self, event_index):
-        self.event_list[event_index].pack_forget()
-        del self.event_list[event_index]
-        self.redraw_event_frames()
+        if len(self.event_list) > 1:
+            self.event_list[event_index].event_frame.pack_forget()
+            del self.event_list[event_index]
+            self.redraw_event_frames()
 
     def redraw_event_frames(self):
-        for event_frame in self.event_list.values():
-            event_frame.pack(padx=5, pady=10)
+        for event in self.event_list.values():
+            event.event_frame.pack(padx=5, pady=10)
+
+    def load_events(self):
+        ...
+        # todo: load temp_hk etc
+
+    def save_event(self):
+        ...
+
+    def change_events_state(self, event_index):
+        # make sure there is only one active state across all objects!
+        for index, event in self.event_list.items():
+            if index is not event_index:
+                event.state = None
+
+    def on_key_press_callback(self, key):
+        #check which object is in an active state
+        for index, event in self.event_list.items():
+            if event.state is not None:
+                if event.state == "ch_hotkey":
+                    event.change_hotkey(key)
+                    event.modified = True
+                    event.state = None
 
 
-class EventFrame(tk.Frame):
+class Event:
+    class EventFrame(tk.Frame):
+        def __init__(self, parent_window, event_manager, event_index, *args, **kwargs):
+            tk.Frame.__init__(self, parent_window, *args, **kwargs)
+
+            self.event_manager = event_manager
+            self.event_index = event_index
+            # possible states: None, "ch_hotkey", "ch_key_to_send"..think of more
+
+            # entry hotkey
+            self.hk_entry_var = tk.StringVar()
+            self.hk_entry = tk.Entry(self, textvariable=self.hk_entry_var)
+            self.hk_entry['state'] = 'readonly'
+
+            # entry key to send
+            self.key_to_send_entry_var = tk.StringVar()
+            self.key_to_send_entry = tk.Entry(self, textvariable=self.key_to_send_entry_var)
+            self.key_to_send_entry['state'] = 'readonly'
+
+            # combobox to choose action
+            self.action_cb_var = tk.StringVar()
+            self.action_cb = ttk.Combobox(self, textvariable=self.action_cb_var)
+            self.action_cb['values'] = ["Continuous", "1 sec delay", "2 sec delay"]
+            self.action_cb['state'] = 'readonly'
+            self.action_cb.set(self.action_cb['values'][0])
+
+            # button plus
+            self.plus_button = tk.Button(self, text='Add', width=10,
+                                         command=lambda: EventManager.add_event(event_manager))
+
+            # button minus
+            self.minus_button = tk.Button(self, text='Remove', width=10,
+                                          command=lambda: EventManager.remove_event(event_manager, self.event_index))
+
+            # CALLBACKS
+            self.hk_entry.bind('<Button-1>', self.hk_entry_focus_callback)
+
+            # pack everything
+            self.hk_entry.pack(side='left', padx=(10, 5))
+            self.key_to_send_entry.pack(side='left', padx=5)
+            self.action_cb.pack(side='left', padx=5)
+            self.plus_button.pack(side='left', padx=5)
+            self.minus_button.pack(side='left', padx=(5, 10))
+            self.pack(padx=5, pady=10)
+
+        def hk_entry_focus_callback(self, mouse_pointer):
+            event = self.event_manager.event_list[self.event_index]
+            if event.state != "ch_hotkey":
+                self.temp_hk = self.hk_entry_var.get()
+                self.hk_entry_var.set('Change hotkey...')
+                event.state = "ch_hotkey"
+                self.event_manager.change_events_state(self.event_index)
+            else:
+                event.state = None
+
     def __init__(self, parent_window, event_manager, event_index, *args, **kwargs):
-        tk.Frame.__init__(self, parent_window, *args, **kwargs)
-
+        # create a frame
+        self.event_frame = self.EventFrame(parent_window, event_manager, event_index, *args, **kwargs)
         self.event_index = event_index
+        self.action_type = self.event_frame.action_cb_var
+        self.key_to_send = self.event_frame.key_to_send_entry_var
+        self.hotkey = self.event_frame.hk_entry_var
 
-        # entry hotkey
-        self.hk_entry_var = tk.StringVar()
-        self.hk_entry = tk.Entry(self, textvariable=self.hk_entry_var)
-        self.hk_entry['state'] = 'readonly'
+        # properties
+        self._state = None
+        self._modified = False
 
-        # entry key to send
-        self.key_to_send_entry_var = tk.StringVar()
-        self.key_to_send_entry = tk.Entry(self, textvariable=self.key_to_send_entry_var)
-        self.key_to_send_entry['state'] = 'readonly'
+    @property
+    def state(self):
+        return self._state
 
-        # combobox to choose action
-        self.action_cb_var = tk.StringVar()
-        self.action_cb = ttk.Combobox(self, textvariable=self.action_cb_var)
-        self.action_cb['values'] = ["Continuous", "1 sec delay", "2 sec delay"]
-        self.action_cb['state'] = 'readonly'
-        self.action_cb.set(self.action_cb['values'][0])
+    @state.setter
+    def state(self, new_state):
+        if new_state is None:
+            if not self.modified:
+                self.event_frame.hk_entry_var.set(self.event_frame.temp_hk)
+                # todo: needs to add default field changes for other fields
+        self._state = new_state
+        self.modified = False
 
-        # button plus
-        self.plus_button = tk.Button(self, text='Add', width=10,
-                                     command=lambda: EventManager.add_event(event_manager))
+    @property
+    def modified(self):
+        """This property is only true right after a field changes value"""
+        return self._modified
 
-        # button minus
-        self.minus_button = tk.Button(self, text='Remove', width=10,
-                                      command=lambda: EventManager.remove_event(event_manager, self.event_index))
+    @modified.setter
+    def modified(self, bool):
+        self._modified = bool
 
-        # pack everything
-        self.hk_entry.pack(side='left', padx=(10, 5))
-        self.key_to_send_entry.pack(side='left', padx=5)
-        self.action_cb.pack(side='left', padx=5)
-        self.plus_button.pack(side='left', padx=5)
-        self.minus_button.pack(side='left', padx=(5, 10))
-        self.pack(padx=5, pady=10)
+    def bind_action_to_hotkey(self):
+        ...
+
+    def action(self):
+        # aka Call()
+        ...
+
+    def change_hotkey(self, new_hotkey):
+        self.event_frame.hk_entry_var = new_hotkey
+
+    def change_key_to_send(self, new_key_to_send):
+        self.event_frame.key_to_send_entry_var = new_key_to_send
+
--- main.py
+++ main.py
@@ -1,32 +1,11 @@
-import keyboard
-import threading
 import tkinter as tk
 import events
-from time import sleep
 
-
-# TODO:
-# 1) decorator to register events. list with events
-# 2) show events on form. def update_events
-# 3) buttons on form to delete and add events
-# 4) modifier (also default modifier like ctrl) to start event
-# 5) possibly record macro thing
-# 6) each event is a class with a method
-# 7) GUI is a class with a method to register events
-# 8) Save events as functions using partial, or as classes with method Call. Should be class because then I can change
-#           parameters easy at an time
-# 9) Each EventFrame is an object of a class, that can draw itself and self replicate with a decorator
-# 10) Each EventFrame belongs to Event and is created when Event is created
-
-
-
+#todo: rename Event and EventFrame to something more suitable
 
 if __name__ == '__main__':
     root = tk.Tk()
     main_frame = events.MainFrame(root).pack()
     event_manager = events.EventManager(main_frame)
     event_manager.add_event()
-    event_manager.add_event()
-    sleep(1)
-    event_manager.redraw_event_frames()
     root.mainloop()
